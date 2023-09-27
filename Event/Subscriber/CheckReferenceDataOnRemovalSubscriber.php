2224266d1310199068cd9141940b1a29d90df608
Thomas Brankaer <thomas.brankaer@induxx.be>
2023-09-27 10:48:53 +0200

    Bugfix AK7.0

--- Event/Subscriber/CheckReferenceDataOnRemovalSubscriber.php
+++ Event/Subscriber/CheckReferenceDataOnRemovalSubscriber.php
@@ -2,8 +2,8 @@
 
 namespace Pim\Bundle\CustomEntityBundle\Event\Subscriber;
 
-use Akeneo\Channel\Component\Repository\ChannelRepositoryInterface;
-use Akeneo\Channel\Component\Repository\LocaleRepositoryInterface;
+use Akeneo\Channel\Infrastructure\Component\Repository\ChannelRepositoryInterface;
+use Akeneo\Channel\Infrastructure\Component\Repository\LocaleRepositoryInterface;
 use Akeneo\Pim\Enrichment\Component\Product\Query\Filter\Operators;
 use Akeneo\Pim\Enrichment\Component\Product\Query\ProductQueryBuilderFactoryInterface;
 use Akeneo\Pim\Structure\Component\Model\AttributeInterface;
